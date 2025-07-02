ae609eef17bf5c31f00df4a0dd1bafe02f8ba0c5
KipodHater <ethandror123@gmail.com>
2025-07-02 12:55:24 +0300

    uploaded a ton of files

---
+++ bin/generated-sources/annotations/frc/robot/subsystems/gripper/GripperIOInputsAutoLogged.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.gripper;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public class GripperIOInputsAutoLogged extends GripperIO.GripperIOInputs
+    implements LoggableInputs, Cloneable {
+  @Override
+  public void toLog(LogTable table) {
+    table.put("MotorConnected", motorConnected);
+    table.put("GripperMotorRPM", gripperMotorRPM);
+    table.put("GripperMotorVoltage", gripperMotorVoltage);
+    table.put("GripperMotorTemp", gripperMotorTemp);
+  }
+
+  @Override
+  public void fromLog(LogTable table) {
+    motorConnected = table.get("MotorConnected", motorConnected);
+    gripperMotorRPM = table.get("GripperMotorRPM", gripperMotorRPM);
+    gripperMotorVoltage = table.get("GripperMotorVoltage", gripperMotorVoltage);
+    gripperMotorTemp = table.get("GripperMotorTemp", gripperMotorTemp);
+  }
+
+  public GripperIOInputsAutoLogged clone() {
+    GripperIOInputsAutoLogged copy = new GripperIOInputsAutoLogged();
+    copy.motorConnected = this.motorConnected;
+    copy.gripperMotorRPM = this.gripperMotorRPM;
+    copy.gripperMotorVoltage = this.gripperMotorVoltage;
+    copy.gripperMotorTemp = this.gripperMotorTemp;
+    return copy;
+  }
+}
---
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ReefscapeOffseasonBU";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "30d651859d9034c48f1112e0681d9e40544545f9";
+  public static final String GIT_DATE = "2025-07-02 05:50:38 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-07-02 05:54:23 EDT";
+  public static final long BUILD_UNIX_TIME = 1751450063233L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
