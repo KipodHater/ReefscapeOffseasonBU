5e988a3c68228f9f60913d56510079d25c6d6e00
KipodHater <ethandror123@gmail.com>
2025-07-02 12:55:24 +0300

    uploaded a ton of files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "OffseasonReefscapeBU";
+  public static final String MAVEN_NAME = "ReefscapeOffseasonBU";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 44;
-  public static final String GIT_SHA = "3d0b601ed3a60e3e89c5e3cd85d88a5d62bb0d63";
-  public static final String GIT_DATE = "2025-06-28 07:19:09 EDT";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-07-02 05:46:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1751449586811L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "30d651859d9034c48f1112e0681d9e40544545f9";
+  public static final String GIT_DATE = "2025-07-02 05:50:38 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-07-02 05:54:23 EDT";
+  public static final long BUILD_UNIX_TIME = 1751450063233L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
