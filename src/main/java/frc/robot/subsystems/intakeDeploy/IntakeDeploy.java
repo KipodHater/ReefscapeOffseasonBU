3d3407a21aa13f379dd556e71457c9a7affc4219
KipodHater <ethandror123@gmail.com>
2025-07-07 20:56:51 +0300

    added two intake subsystems

---
+++ src/main/java/frc/robot/subsystems/intakeDeploy/IntakeDeploy.java
@@ -0,0 +1,66 @@
+package frc.robot.subsystems.intakeDeploy;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+
+public class IntakeDeploy {
+    private final IntakeDeployIO io;
+    private final IntakeDeployIOInputsAutoLogged inputs = new IntakeDeployIOInputsAutoLogged();
+
+    private final ArmFeedforward feedforwardController =
+        new ArmFeedforward(IntakeDeployConstants.GAINS.KS(), IntakeDeployConstants.GAINS.KG(), IntakeDeployConstants.GAINS.KV());
+    
+    public enum IntakeDeployStates {
+        CLOSED(0.0),
+        DEPLOY(90.0),
+        RETRACT(0.0),
+        IDLE(null);
+
+        Double value;
+
+        IntakeDeployStates(Double value) {
+            this.value = value;
+        }
+
+        public Double position() {
+            return this.value;
+        }
+    };
+
+    @AutoLogOutput(key = "IntakeDeploy/currentState")
+    private IntakeDeployStates currentState = IntakeDeployStates.IDLE;
+
+    public IntakeDeploy(IntakeDeployIO io) {
+        this.io = io;
+    }
+
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("IntakeDeploy/inputs", inputs);
+
+        stateMachine();
+    }
+
+    private void stateMachine(){
+        double ffVoltage = feedforwardController.calculate(currentState.position()*(Math.PI / 180.0), inputs.velocityDegPerSec * (Math.PI / 180.0));
+                 // Convert velocity to radians per second
+
+        switch (currentState) {
+            case CLOSED -> io.runPosition(IntakeDeployStates.CLOSED.position(), ffVoltage);
+
+            case DEPLOY -> io.runPosition(IntakeDeployStates.DEPLOY.position(), ffVoltage);
+
+            case RETRACT -> io.runPosition(IntakeDeployStates.RETRACT.position(), ffVoltage);
+
+            case IDLE -> io.stop();
+
+            default -> io.stop();
+        }
+    }
+
+    public void setBrakeMode(boolean isBrake) {
+        io.setBrakeMode(isBrake);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/intakeDeploy/IntakeDeployIO.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.intakeDeploy;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeDeployIO {
+    @AutoLog
+    public static class IntakeDeployIOInputs {
+        public boolean motorConnected = false;
+
+        public double positionDeg = 0;
+        public double velocityDegPerSec = 0;
+        public double motorVoltage = 0; // volts
+        public double motorTemp = 0; // celsius
+        public double motorCurrent = 0; // amps
+    }
+
+    public default void updateInputs(IntakeDeployIOInputs inputs) {}
+
+    public default void runVoltage(double voltage) {}
+
+    public default void stop() {}
+
+    public default void runPosition(double position, double feedforward) {}
+
+    public default void setPID(double KP, double KI, double KD) {}
+
+    public default void setBrakeMode(boolean isBrake) {}
+
+    public default void setSoftLimits(double forwardLimit, double reverseLimit) {}
+}
