63eb3dd96da8e8fe32347b5680bff4c607ee3827
roy2872 <roylevy1204@gmail.com>
2025-08-14 20:53:15 +0300

    added dashboard, example set in container even added fancy descriptions :3

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ReefscapeOffseasonBU";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 45;
-  public static final String GIT_SHA = "c352878b7af35bad1de9c4e71f0bf984f19d7e87";
-  public static final String GIT_DATE = "2025-08-02 07:13:00 EDT";
-  public static final String GIT_BRANCH = "controllers";
-  public static final String BUILD_DATE = "2025-08-10 12:10:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1754842248058L;
+  public static final int GIT_REVISION = 50;
+  public static final String GIT_SHA = "f4c997a8b2ea195a42c8a8cf28f4ec422676e49b";
+  public static final String GIT_DATE = "2025-08-14 06:54:23 EDT";
+  public static final String GIT_BRANCH = "dashboard";
+  public static final String BUILD_DATE = "2025-08-14 13:50:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1755193856881L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
