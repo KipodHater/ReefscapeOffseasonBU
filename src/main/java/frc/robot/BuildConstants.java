c0303e6374833381eb2afbca48b9323621dff6cb
KipodHater <ethandror123@gmail.com>
2025-08-24 14:05:41 +0300

    finished intake logic, still missing ignore sensors

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ReefscapeOffseasonBU";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 50;
-  public static final String GIT_SHA = "f4c997a8b2ea195a42c8a8cf28f4ec422676e49b";
-  public static final String GIT_DATE = "2025-08-14 06:54:23 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-08-15 06:39:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1755254369759L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "04f3d809102d54bedbddbdde76e3788b83bda6c8";
+  public static final String GIT_DATE = "2025-08-15 06:44:27 EDT";
+  public static final String GIT_BRANCH = "intake-logic";
+  public static final String BUILD_DATE = "2025-08-24 07:04:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1756033441867L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
