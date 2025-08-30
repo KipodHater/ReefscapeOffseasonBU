8ddfda49ef3687c6052916864d1afdc3a2df6d2b
KipodHater <ethandror123@gmail.com>
2025-08-30 13:07:17 +0300

    found a nice camera placement but need to test more

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ReefscapeOffseasonBU";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 60;
-  public static final String GIT_SHA = "c5f0596f7341b4168742b9125fa3a3fcc48dd139";
-  public static final String GIT_DATE = "2025-08-29 11:02:01 EDT";
+  public static final int GIT_REVISION = 61;
+  public static final String GIT_SHA = "f65a33e238c2d9ade3e236bd2cbf20eb0ff3758d";
+  public static final String GIT_DATE = "2025-08-29 11:38:39 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-08-29 11:14:35 EDT";
-  public static final long BUILD_UNIX_TIME = 1756480475058L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-08-30 06:00:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1756548010106L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
